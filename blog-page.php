e4538af826ae73c2e375a8fa13f73218a4eafcbe
aarav-tech-123 <developer@aaravtech.net>
2025-11-20 22:53:19 +0530

    pages fixed

--- blog-page.php
+++ blog-page.php
@@ -914,7 +914,7 @@ $stmt_meta->close();
                 <div class="footer-bottom-links">
                     <a href="https://aaravtech.net/privacy-policy.html">Privacy Policy</a>
                     <a href="https://aaravtech.net/terms-and-conditions.html">Terms of Service</a>
-                    <a href="sitemap.html">Sitemap</a>
+                    <a href="https://aaravtech.net/sitemap.html">Sitemap</a>
                 </div>
             </div>
         </div>
